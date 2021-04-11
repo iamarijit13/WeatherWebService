3bd3587b31afe414f424ebf14d956969efa3dcd3
ARIJIT <68080361+arispeak@users.noreply.github.com>
2021-04-11 13:54:21 +0530

    Unwanted mistake rectify.

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@
 //  console.log(path.join(__dirname,'../public'));
 
  const app = express();
- const port = express.env.PORT || 3000
+ const port = process.env.PORT || 3000
 
 
  //Define paths for express configaration.
